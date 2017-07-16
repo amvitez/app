16b5800d6243b623b6454d0db92b643eb997bee3
Tony Vitez <amvitez@gmail.com>
2017-07-15 22:37:56 -0700

    l

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/ingredients', function (req, res, next) {
 });
 
 app.post('/import-ingredients', function (req, res, next) {
-	ingredients.insert(req.body.data);
+	db.ingredients.insert(req.body.data);
 });
 
 app.listen(process.env.PORT || 3000, function () {
