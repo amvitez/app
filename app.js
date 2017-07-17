4b4f5ac67b62ea579726ff3ac3a4511b8b2eddfe
Tony Vitez <amvitez@gmail.com>
2017-07-16 17:46:44 -0700

    l

--- app.js
+++ app.js
@@ -52,54 +52,51 @@ app.post('/import-ingredients', function (req, res, next) {
     //var i = {
     //	name: req.body[0].name
     //};
+    try {
+	    var data = req.body;
 
-    var data = req.body;
+	    for (var j = 0; j < data.length; j++) {
+	    	var a = data[j];
+	    	var s = a.serving;
+	    	var ingredientId;
 
-    for (var j = 0; j < data.length; j++) {
-    	var a = data[j];
-    	var s = a.serving;
-    	var ingredientId;
+	    	// check ingredients for name
+	    	var exists = ingredients.find({name: a.ingredient});
 
-    	// check ingredients for name
-    	var exists = ingredients.find({name: a.ingredient});
-
-    	if (exists) {
-    		ingredientId = exists._id;
-    		var newServing = {
-			ingredientId: ingredientId,
-			unit: s,
-			nutrition: a.nutrition 
-		};
-
-		servings.insert(newServing);
-    	} else {
-    		var ingredient = {
-	    		name: a.ingredient
-	    	} 
-
-	    	ingredients.insert(ingredient, function(err){
-			   if (err) return;
-			   // Object inserted successfully.
-			   ingredientId = ingredient._id; // this will return the id of object inserted
-			   var newServing = {
+	    	if (exists) {
+	    		ingredientId = exists._id;
+	    		var newServing = {
 					ingredientId: ingredientId,
 					unit: s,
 					nutrition: a.nutrition 
 				};
 
 				servings.insert(newServing);
-			});
-    	}
-    	
-		
-    }
+	    	} else {
+	    		var ingredient = {
+		    		name: a.ingredient
+		    	} 
 
-    //if name exists, delete it and all servings for it
-    //insert name into INGREDIENTS, get _id and for each serving insert _id, name, nutrition into SERVINGS
+		    	ingredients.insert(ingredient, function(err){
+				   if (err) return;
+				   // Object inserted successfully.
+				   ingredientId = ingredient._id; // this will return the id of object inserted
+				   var newServing = {
+						ingredientId: ingredientId,
+						unit: s,
+						nutrition: a.nutrition 
+					};
 
-	ingredients.insert(i, function(err, res) {
-		console.log(err);
-	});
+					servings.insert(newServing);
+				});
+	    	}
+	    }
+	} catch(err) {
+		res.render('ingredients', {
+			ingredients: [],
+			error: err
+		});
+	}
 });
 
 app.listen(process.env.PORT || 3000, function () {
