049fdb9a776ef7cd9e7396d8b5bd12e49a4dd54e
Tony Vitez <amvitez@gmail.com>
2017-07-16 17:17:21 -0700

    l

--- app.js
+++ app.js
@@ -26,10 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 var uri = 'mongodb://heroku_93hrxj6h:fnq24dnqc0lpqvo9l9m7f8rf4v@ds161162.mlab.com:61162/heroku_93hrxj6h';
 var db;
 var ingredients;
+var servings;
 
 mongodb.MongoClient.connect(uri, function(err, _db) { 
 	db = _db;
 	ingredients = db.collection('ingredients');
+	servings = db.collection('servings');
 });
 
 app.get('/', function (req, res, next) {
@@ -47,9 +49,53 @@ app.get('/ingredients', function (req, res, next) {
 });
 
 app.post('/import-ingredients', function (req, res, next) {
-    var i = {
-    	name: req.body[0].name
-    };
+    //var i = {
+    //	name: req.body[0].name
+    //};
+
+    var data = req.body;
+
+    for (var j = 0; j < data.length; j++) {
+    	var a = data[j];
+    	var s = a.serving;
+    	var ingredientId;
+
+    	// check ingredients for name
+    	var exists = ingredients.find({name: a.ingredient});
+
+    	if (exists) {
+    		ingredientId = exists._id;
+    		var newServing = {
+			ingredientId: ingredientId,
+			unit: s,
+			nutrition: a.nutrition 
+		};
+
+		servings.insert(newServing);
+    	} else {
+    		var ingredient = {
+	    		name: a.ingredient
+	    	} 
+
+	    	ingredients.insert(ingredient, function(err){
+			   if (err) return;
+			   // Object inserted successfully.
+			   ingredientId = ingredient._id; // this will return the id of object inserted
+			   var newServing = {
+					ingredientId: ingredientId,
+					unit: s,
+					nutrition: a.nutrition 
+				};
+
+				servings.insert(newServing);
+			});
+    	}
+    	
+		
+    }
+
+    //if name exists, delete it and all servings for it
+    //insert name into INGREDIENTS, get _id and for each serving insert _id, name, nutrition into SERVINGS
 
 	ingredients.insert(i, function(err, res) {
 		console.log(err);
